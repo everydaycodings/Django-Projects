a2389a0df7525ae94dd07c1c44b003762ac08d33
saksham <sakshammaxwell1891@gmail.com>
2020-09-01 10:29:25 +0530

    DEBUG = FALSE

--- Projects/settings.py
+++ Projects/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'ch9koy0czfn_7+#+t9n6%1g#wkcleqpzv7)#3ma3w%b0k=nnc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
